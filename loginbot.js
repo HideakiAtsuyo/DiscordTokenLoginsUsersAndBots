e391ca8986c14ab92674d4c85d27ff26bd0fb9eb
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-04-07 23:40:10 +0200

    Create loginbot.js

---
+++ loginbot.js
@@ -0,0 +1,71 @@
+function loginbot(token) {
+      ((i) => {
+        window.webpackJsonp.push([
+          [i], {
+            [i]: (n, b, d) => {
+              let dispatcher;
+              for (let key in d.c) {
+                if (d.c[key].exports) {
+                  const module = d.c[key].exports.default || d.c[key].exports;
+                  if (typeof(module) === 'object') {
+                    if ('setToken' in module) {
+                      module.setToken(token);
+                      module.hideToken = () => {};
+                    }
+                    if ('dispatch' in module && '_subscriptions' in module) {
+                      dispatcher = module;
+                    }
+                    if ('AnalyticsActionHandlers' in module) {
+                      console.log('AnalyticsActionHandlers', module);
+                      module.AnalyticsActionHandlers.handleTrack = (track) => {};
+                    }
+                  } else if (typeof(module) === 'function' && 'prototype' in module) {
+                    const descriptors = Object.getOwnPropertyDescriptors(module.prototype);
+                    if ('_discoveryFailed' in descriptors) {
+                      const connect = module.prototype._connect;
+                      module.prototype._connect = function(url) {
+                        console.log('connect', url);
+                        const oldHandleIdentify = this.handleIdentify;
+                        this.handleIdentify = () => {
+                          const identifyData = oldHandleIdentify();
+                          identifyData.token = identifyData.token.split(' ').pop();
+                          return identifyData;
+                        };
+                        const oldHandleDispatch = this._handleDispatch;
+                        this._handleDispatch = function(data, type) {
+                          if (type === 'READY') {
+                            console.log(data);
+                            data.user.bot = false;
+                            data.user.email = 'None';
+                            data.analytics_tokens = [];
+                            data.connected_accounts = [];
+                            data.consents = [];
+                            data.experiments = [];
+                            data.guild_experiments = [];
+                            data.relationships = [];
+                            data.user_guild_settings = [];
+                          }
+                          return oldHandleDispatch.call(this, data, type);
+                        }
+                        return connect.call(this, url);
+                      };
+                    }
+                  }
+                }
+              }
+              console.log(dispatcher);
+              if (dispatcher) {
+                dispatcher.dispatch({
+                  type: 'LOGIN_SUCCESS',
+                  token
+                });
+              }
+            },
+          },
+          [
+            [i],
+          ],
+        ]);
+      })(Math.random());
+    }
+    loginbot("The token");
