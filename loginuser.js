afc2dff8ffbd50bcf8be5829fb86de14f684972f
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-04-07 23:38:58 +0200

    Create loginuser.js

---
+++ loginuser.js
@@ -0,0 +1,9 @@
+function loginuser(token) {
+setInterval(() => {
+document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+}, 50);
+setTimeout(() => {
+location.reload();
+}, 2500);
+}
+loginuser("The token")
